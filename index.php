1dd15a432e7a4863c7c1b2dbacec91c23dcd8280
subham <karmakarsubham194@gmail.com>
2026-08-30 01:04:24 +0530

    Fix: include rate_limiter.php in front controller

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/config/mailer.php';
 require_once __DIR__ . '/includes/auth.php';
+require_once __DIR__ . '/includes/rate_limiter.php';
 
 startSession();
 
